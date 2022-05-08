fbab5ae85b276b01b81d2b2abc227f50bd283843
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-05-07 23:15:10 -0300

    update

--- 03-javascript-interfaces-e-heranca/index.js
+++ 03-javascript-interfaces-e-heranca/index.js
@@ -6,17 +6,17 @@ import { Conta } from './Conta.js'
 const cliente1 = new Cliente('Marcelo', 12345678900, 123)
 const cliente2 = new Cliente('Ricardo', 32165498700, 321)
 
-// --------------------------------------------------------------------
+// ------------------------------------------------------------------
 
 const contaCorrenteMarcelo = new Conta(1, 1001, 5001, cliente1)
 const contaCorrenteRicardo = new Conta(2, 1001, 5002, cliente2)
 
-// --------------------------------------------------------------------
+// ------------------------------------------------------------------
 
 const contaPoupancaMarcelo = new Conta(3, 1001, 5011, cliente1)
 const contaPoupancaRicardo = new Conta(4, 1001, 5022, cliente2)
 
-// --------------------------------------------------------------------
+// ------------------------------------------------------------------
 
 // contaCorrenteMarcelo.depositar(100)
 // contaCorrenteMarcelo.sacar(50)
@@ -26,13 +26,13 @@ const contaPoupancaRicardo = new Conta(4, 1001, 5022, cliente2)
 
 // contaCorrenteMarcelo.transferir(1, contaCorrenteRicardo)
 
-// --------------------------------------------------------------------
+// ------------------------------------------------------------------
 
 console.log(contaCorrenteMarcelo)
 console.log(contaCorrenteRicardo)
 // console.log(`Numero de Contas: ${ContaCorrente.numeroDeContas}`);
 
-// --------------------------------------------------------------------
+// ------------------------------------------------------------------
 
 console.log(contaPoupancaMarcelo)
 console.log(contaPoupancaRicardo)
