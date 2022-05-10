a3a30c3f34edd049c3fb342649b6674cd28527ad
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-05-10 13:11:19 -0300

    update script.js

--- 00-html5-ccs3-introducao/js/script.js
+++ 00-html5-ccs3-introducao/js/script.js
@@ -1,2 +1,30 @@
-var name = 'marcelo'
-console.log(name)
+const myName = 'marcelo'
+console.log(myName)
+
+// -------------------------
+function myName2 () {
+  console.log('marcelo')
+}
+
+myName2()
+
+// -------------------------
+const myName3 = () => {
+    console.log('marcelo')
+}
+
+myName3()
+
+// -------------------------
+function myName4 (params) {
+  console.log(params)
+}
+
+myName4('marcelo')
+
+// -------------------------
+const myName5 = (args) => {
+  console.log(args)
+}
+
+myName5('marcelo')
