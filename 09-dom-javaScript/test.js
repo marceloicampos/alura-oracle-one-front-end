cd96d0f46f99b2174a9a9b2f0bd73d6d747893a8
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-05-31 14:10:39 -0300

    conhecendo o DOM

---
+++ 09-dom-javaScript/test.js
@@ -0,0 +1,9 @@
+var item = document.querySelector('.content').textContent
+var item = 'comprar maça'
+console.log(item)
+document.querySelector('.content').textContent = item
+
+// document.getElementById(‘id’) seleciona o elemento pelo id passado.
+// document.getElementsByClassName(‘classe’) retorna um array dos elementos pelo nome da classe passada.
+// document.getElementsByTagName(‘tag’) retorna um array dos elementos pelo nome da tag passada
+// document.querySelectorAll(seletor) devolve todos os seletores com o mesmo nome
