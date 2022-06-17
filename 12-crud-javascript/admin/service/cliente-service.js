632e5ce02530a85eba9bab4aeb6a0c106e770bcd
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-06-17 12:37:33 -0300

    projeto inicial

---
+++ 12-crud-javascript/admin/service/cliente-service.js
@@ -0,0 +1,29 @@
+// inside folder admin exec npx json-server --watch db.json
+
+const criaNovaLinha = (nome, email) => {
+    const linhaNovoCliente = document.createElement('tr')
+    const conteudo = `
+  <td class="td" data-td>${nome}</td>
+  <td>${email}</td>
+  <td>
+      <ul class="tabela__botoes-controle">
+          <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+          <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+      </ul>
+  </td>
+  `
+    linhaNovoCliente.innerHTML = conteudo
+    return linhaNovoCliente
+}
+
+const tabela = document.querySelector('[data-tabela')
+
+const http = new XMLHttpRequest()
+http.open('GET', 'http://localhost:3000/profile')
+http.send()
+http.onload = () => {
+    const data = JSON.parse(http.response)
+    data.forEach (element => {
+        tabela.appendChild(criaNovaLinha(element.nome, element.email))
+    })
+}
