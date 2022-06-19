7b8e7a82f5f8490312e5c249456ca8e5a2c134eb
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-06-19 19:15:57 -0300

    Update cliente-service.js

--- 12-crud-javascript/admin/service/cliente-service.js
+++ 12-crud-javascript/admin/service/cliente-service.js
@@ -3,15 +3,15 @@
 const criaNovaLinha = (nome, email) => {
     const linhaNovoCliente = document.createElement('tr')
     const conteudo = `
-  <td class="td" data-td>${nome}</td>
-  <td>${email}</td>
-  <td>
-      <ul class="tabela__botoes-controle">
-          <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-          <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-      </ul>
-  </td>
-  `
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+        <ul class="tabela__botoes-controle">
+            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+        </ul>
+    </td>
+`
     linhaNovoCliente.innerHTML = conteudo
     return linhaNovoCliente
 }
@@ -23,7 +23,7 @@ http.open('GET', 'http://localhost:3000/profile')
 http.send()
 http.onload = () => {
     const data = JSON.parse(http.response)
-    data.forEach (element => {
+    data.forEach(element => {
         tabela.appendChild(criaNovaLinha(element.nome, element.email))
     })
 }
