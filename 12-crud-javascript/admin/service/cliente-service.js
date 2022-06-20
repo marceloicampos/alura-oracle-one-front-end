3959bc76f49f47ead2f5093c6daec636c6115962
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-06-20 00:26:58 -0300

    implementa async fetch api

--- 12-crud-javascript/admin/service/cliente-service.js
+++ 12-crud-javascript/admin/service/cliente-service.js
@@ -18,12 +18,39 @@ const criaNovaLinha = (nome, email) => {
 
 const tabela = document.querySelector('[data-tabela')
 
-const http = new XMLHttpRequest()
-http.open('GET', 'http://localhost:3000/profile')
-http.send()
-http.onload = () => {
-    const data = JSON.parse(http.response)
+// COM FUNÇÃO ASSÍNCRONA
+const listaClientes = async () => {
+    const response = await fetch(`http://localhost:3000/profile`)
+    return await response.json()
+}
+
+listaClientes().then(data => {
     data.forEach(element => {
         tabela.appendChild(criaNovaLinha(element.nome, element.email))
     })
-}
+})
+
+// SEM PROMISE COM FUNÇÃO SÍNCRONA
+// const listaClientes = () => {
+//     return fetch(`http://localhost:3000/profile`).then(response => {
+//         return response.json()
+//     })
+// }
+
+// COM PROMISE
+// const listaClientes = () => {
+//     const promise = new Promise((resolve, reject) => {
+//         const http = new XMLHttpRequest()
+//         http.open('GET', 'http://localhost:3000/profile')
+//         http.onload = () => {
+//             if (http.status >= 400) {
+//                 reject(JSON.parse(http.response))
+//             } else {
+//                 resolve(JSON.parse(http.response))
+//             }
+//         }
+//         http.send()
+//     })
+//     // console.log(promise)
+//     return promise
+// }
