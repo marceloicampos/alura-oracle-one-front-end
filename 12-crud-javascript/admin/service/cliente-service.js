e61b2fd55e3ffc21d3ecb21f987d0f6d0462c1bb
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-06-20 00:55:50 -0300

    criando controller

--- 12-crud-javascript/admin/service/cliente-service.js
+++ 12-crud-javascript/admin/service/cliente-service.js
@@ -1,36 +1,16 @@
 // inside folder admin exec npx json-server --watch db.json
 
-const criaNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `
-    <td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td>
-        <ul class="tabela__botoes-controle">
-            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-        </ul>
-    </td>
-`
-    linhaNovoCliente.innerHTML = conteudo
-    return linhaNovoCliente
-}
-
-const tabela = document.querySelector('[data-tabela')
-
 // COM FUNÇÃO ASSÍNCRONA
 const listaClientes = async () => {
     const response = await fetch(`http://localhost:3000/profile`)
     return await response.json()
 }
 
-listaClientes().then(data => {
-    data.forEach(element => {
-        tabela.appendChild(criaNovaLinha(element.nome, element.email))
-    })
-})
+export const clienteService = {
+    listaClientes
+}
 
-// SEM PROMISE COM FUNÇÃO SÍNCRONA
+// COM FUNÇÃO SÍNCRONA
 // const listaClientes = () => {
 //     return fetch(`http://localhost:3000/profile`).then(response => {
 //         return response.json()
