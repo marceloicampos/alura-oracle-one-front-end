7f9c2b5c7ac31d61cc6bdea0fd2200f73f8d9ad9
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-06-21 23:30:00 -0300

    validacao campos nome, email, senha, dataNascimento

--- 13-forms-javascript/dogito/validacao.js
+++ 13-forms-javascript/dogito/validacao.js
@@ -9,10 +9,47 @@ export function valida(input) {
     if (validadores[tipoDeInput]) {
         validadores[tipoDeInput](input)
     }
+    if (input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
+}
+
+const tiposDeErro = ['valueMissing', 'typeMismatch', 'patternMismatch', 'customError']
+
+const mensagemDeErros = {
+    nome: {
+        valueMissing: 'O Campo Nome não pode estar vazio'
+    },
+    email: {
+        valueMissing: 'O Campo Email não pode estar vazio',
+        typeMismatch: 'O Email não é valido'
+    },
+    senha: {
+        valueMissing: 'O Campo Senha não pode estar vazio',
+        patternMismatch: 'A senha deve conter pelo menos 1 número, 1 letra minúscula e 1 letra maiúscula e deve ter entre 6 a 8 caracteres'
+    },
+    dataNascimento: {
+        valueMissing: 'O Campo Data não pode estar vazio',
+        customError: 'Você deve ser maior de 18 anos para efetuar cadastro'
+    }
 }
 
 const validadores = {
     dataNascimento: input => validaDataNascimento(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if (input.validity[erro]) {
+            mensagem = mensagemDeErros[tipoDeInput][erro]
+        }
+    })
+    return mensagem
 }
 
 function validaDataNascimento(input) {
@@ -27,7 +64,7 @@ function validaDataNascimento(input) {
 function maiorQue18(data) {
     const dataAtual = new Date()
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
-    console.log(dataMais18)
+    // console.log(dataMais18)
 
     return dataMais18 <= dataAtual
 }
